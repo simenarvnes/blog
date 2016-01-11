a9fea67bcd1449779990c7374506296448ebc311
Simen Arvnes <simenarvnes@gmail.com>
2016-01-11 10:35:50 +0100

    test

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,84 @@
+<?php namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Input;
+use Redirect;
+use App\Project;
+use App\Task;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @param  \App\Project $project
+	 * @return Response
+	 */
+	public function index(Project $project)
+	{
+		return view('tasks.index', compact('project'));
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @param  \App\Project $project
+	 * @return Response
+	 */
+	public function create(Project $project)
+	{
+		return view('tasks.create', compact('project'));
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Project $project
+	 * @param  \App\Task    $task
+	 * @return Response
+	 */
+	public function show(Project $project, Task $task)
+	{
+		return view('tasks.show', compact('project', 'task'));
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Project $project
+	 * @param  \App\Task    $task
+	 * @return Response
+	 */
+	public function edit(Project $project, Task $task)
+	{
+		return view('tasks.edit', compact('project', 'task'));
+	}
+
+	public function store(Project $project)
+	{
+		$input = Input::all();
+		$input['project_id'] = $project->id;
+		Task::create( $input );
+
+		return Redirect::route('projects.show', $project->slug)->with('message', 'Task created.');
+	}
+
+	public function update(Project $project, Task $task)
+	{
+		$input = array_except(Input::all(), '_method');
+		$task->update($input);
+
+		return Redirect::route('projects.tasks.show', [$project->slug, $task->slug])->with('message', 'Task updated.');
+	}
+
+	public function destroy(Project $project, Task $task)
+	{
+		$task->delete();
+
+		return Redirect::route('projects.show', $project->slug)->with('message', 'Task deleted.');
+	}
+
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Provide controller methods with object instead of ID
+Route::model('tasks', 'Task');
+Route::model('projects', 'Project');
+
+Route::resource('projects', 'ProjectsController');
+Route::resource('projects.tasks', 'TasksController');
+
+//Slug-based URL's
+Route::bind('tasks', function($value, $route) {
+	return App\Task::whereSlug($value)->first();
+});
+Route::bind('projects', function($value, $route) {
+	return App\Project::whereSlug($value)->first();
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});

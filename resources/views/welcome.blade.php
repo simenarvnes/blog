38595d1414675cb4dc5376bb22d33666271e94a2
Simen Arvnes <simenarvnes@gmail.com>
2016-01-11 11:27:40 +0100

    hey

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
                 padding: 0;
                 width: 100%;
                 display: table;
-                font-weight: 20;
+                font-weight: 10;
                 font-family: 'Lato';
             }
 
